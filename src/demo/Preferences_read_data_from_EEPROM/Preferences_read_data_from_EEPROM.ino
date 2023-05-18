efcd6dcfd235d439f326df221974dfc8b50d5ac3
Speed224 <nicola.cremonesi.2000@gmail.com>
2023-05-18 18:57:06 +0200

    Create Preferences_read_data_from_EEPROM.ino

---
+++ src/demo/Preferences_read_data_from_EEPROM/Preferences_read_data_from_EEPROM.ino
@@ -0,0 +1,30 @@
+//https://randomnerdtutorials.com/esp32-save-data-permanently-preferences/
+#include <Preferences.h>
+#include <WiFi.h>
+
+Preferences preferences;
+
+String ssid;
+String password;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  
+  preferences.begin("credentials", false);
+ 
+  ssid = preferences.getString("SSID", ""); 
+  password = preferences.getString("PASSWORD", "");
+
+  if (ssid == "" || password == ""){
+    Serial.println("No values saved for ssid or password");
+  }
+  else {
+    Serial.printf("ssid: %s\n", ssid);
+    Serial.printf("password: %s\n", password);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
