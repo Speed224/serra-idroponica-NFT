91ed1fe01b9c1dcb392ebb197ac07aa9c0a7cf61
Speed224 <nicola.cremonesi.2000@gmail.com>
2023-06-03 15:19:34 +0200

    Create TDS_Data_Read_ESP32.ino

---
+++ src/demo/TDS_Data_Read_ESP32/TDS_Data_Read_ESP32.ino
@@ -0,0 +1,95 @@
+// Original source code: https://wiki.keyestudio.com/KS0429_keyestudio_TDS_Meter_V1.0#Test_Code
+// Project details: https://RandomNerdTutorials.com/arduino-tds-water-quality-sensor/
+
+// working on ESP32 that have 12 bit analog read resolution
+// the online examples are writed for arduino (10 bit analog resolution)
+// 3 easy fix for ESP
+// change maxAnalogValue
+// use map function "map(value, fromLow, fromHigh, toLow, toHigh) --> map(value, 0, 4096, 0 , 1023)" when read the sensor
+// change the analogReadResolution to 10 bit --> "analogReadResolution(10);" after every read if different resolution is needed
+
+
+
+#define TdsSensorPin 34
+#define VREF 3.3              // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30            // sum of sample point
+
+int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
+
+float averageVoltage = 0;
+float tdsValue = 0;
+float temperature = 20;       // current WATER temperature for compensation
+
+const float maxAnalogValue = 4096.0;
+
+// median filtering algorithm
+int getMedianNum(int bArray[], int iFilterLen){
+  int bTab[iFilterLen];
+  for (byte i = 0; i<iFilterLen; i++)
+  bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0){
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  }
+  else {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
+}
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(TdsSensorPin,INPUT);
+  
+  //analogReadResolution(10);                                         //solution three  
+}
+
+void loop(){
+  static unsigned long analogSampleTimepoint = millis();     
+  if(millis()-analogSampleTimepoint > 100U){                          //every 100 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();                                 //solution two map function
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);       //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if(analogBufferIndex == SCOUNT){ 
+      analogBufferIndex = 0;
+    }
+  }   
+  
+  static unsigned long printTimepoint = millis();
+  if(millis()-printTimepoint > 800U){
+    printTimepoint = millis();
+    for(copyIndex=0; copyIndex<SCOUNT; copyIndex++){
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+      
+      // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / maxAnalogValue; //solution one
+      
+      //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0)); 
+      float compensationCoefficient = 1.0+0.02*(temperature-25.0);
+      //temperature compensation
+      float compensationVoltage=averageVoltage/compensationCoefficient;
+      
+      //convert voltage value to tds value
+      tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
+      
+      //Serial.print("voltage:");
+      //Serial.print(averageVoltage,2);
+      //Serial.print("V   ");
+      Serial.print("TDS Value:");
+      Serial.print(tdsValue,0);
+      Serial.println("ppm");
+    }
+  }
+}
